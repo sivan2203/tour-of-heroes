29705ee3eb97915afdcc71b0dbc6afeb4275b310
sivan2203 <sivan9458@gmail.com>
2021-09-29 17:50:24 +0700

    fix

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -15,10 +15,17 @@ export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
 
+  selectedHero?: Hero;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
 }
+
+
